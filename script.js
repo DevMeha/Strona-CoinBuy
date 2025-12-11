e7014cdca3296af2de25806f548d355597b104e6
DevMeha <mixiadami@gmail.com>
2025-12-11 19:31:41 +0100

    COMMENT MORE

--- script.js
+++ script.js
@@ -24,4 +24,23 @@ document.addEventListener('DOMContentLoaded', function() {
             }
         });
     }
+
+
+    function updateClock() {
+        const now = new Date();
+        const hours = now.getHours().toString().padStart(2, '0');
+        const minutes = now.getMinutes().toString().padStart(2, '0');
+        const seconds = now.getSeconds().toString().padStart(2, '0');
+
+        const clock = document.getElementById('clock');
+        if (clock) {
+             clock.textContent = `Aktualna godzina: ${hours}:${minutes}:${seconds}`;
+        }
+    }
+
+    setInterval(updateClock, 1000);
+    updateClock();
+
+
 });
+
